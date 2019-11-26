68a1c10ed04356d3308e7907b7c569801b53785b
Steven A. Rodriguez <hi@stevenarodriguez.com>
2019-11-26 01:10:07 -0500

    update lightbox js

--- assets/scripts/lightbox.js
+++ assets/scripts/lightbox.js
@@ -1,3 +1,16 @@
+/*!
+ * Lightbox v2.11.1
+ * by Lokesh Dhakar
+ *
+ * More info:
+ * http://lokeshdhakar.com/projects/lightbox2/
+ *
+ * Copyright Lokesh Dhakar
+ * Released under the MIT license
+ * https://github.com/lokesh/lightbox2/blob/master/LICENSE
+ *
+ * @preserve
+ */
 
 $(document).ready(function(){
    
