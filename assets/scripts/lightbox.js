cff22348d3035075399b795d927891ab5af89f70
startupweeknyc <58193709+startupweeknyc@users.noreply.github.com>
2019-11-26 01:16:38 -0500

    update lightbox js

--- assets/scripts/lightbox.js
+++ assets/scripts/lightbox.js
@@ -1,16 +1,3 @@
-/*!
- * Lightbox v2.11.1
- * by Lokesh Dhakar
- *
- * More info:
- * http://lokeshdhakar.com/projects/lightbox2/
- *
- * Copyright Lokesh Dhakar
- * Released under the MIT license
- * https://github.com/lokesh/lightbox2/blob/master/LICENSE
- *
- * @preserve
- */
 
 $(document).ready(function(){
    
